6e0ec3c39af70bd40be43f6cfa5daae2f35c4f12
Marcus <marcus.stratu@gmail.com>
2017-06-20 11:25:51 +0300

    Fixed Admin Password;

--- src/Keyhunter/Administrator/Form/Type/Password.php
+++ src/Keyhunter/Administrator/Form/Type/Password.php
@@ -1,25 +1,31 @@
 <?php namespace Keyhunter\Administrator\Form\Type;
 
-class Password extends Text {
+use Keyhunter\Administrator\Form\Element;
 
-	/**
-	 * The specific defaults for the image class
-	 *
-	 * @var array
-	 */
-	protected $passwordDefaults = array(
-		'setter' => true,
-	);
+class Password extends Element  {
 
-	/**
-	 * Gets all default values
-	 *
-	 * @return array
-	 */
-	public function getDefaults()
-	{
-		$defaults = parent::getDefaults();
+    /**
+     * The specific defaults for subclasses to override
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'maxlength' 	=> 255,
+        'class'			=> 'form-control input-sm',
+        'style'			=> 'width: 300px;'
+    ];
 
-		return array_merge($defaults, $this->passwordDefaults);
-	}
+    /**
+     * The specific rules for subclasses to override
+     *
+     * @var array
+     */
+    protected $rules = array(
+        'maxlength' 	=> 'integer|min:0|max:255'
+    );
+
+    public function renderInput()
+    {
+        return \Form::input('password', $this->name, $this->value, $this->attributes);
+    }
 }
